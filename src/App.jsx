1c3de1c097614d7f98ca2d09eab4fce68c5bd233
Diarapak <krobpierre0@gmail.com>
2026-03-26 14:36:02 +0100

    passage sur dummyjson + correction appel api

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import DataTable from './Component/DataTable/DataTable.jsx';
 function App() {
 
   const fetchData = async () => {
-    const data = await fetch("http://localhost:3000/user/1", { method: "POST" })
-    console.log("data :", data)
+    const data = await fetch("https://dummyjson.com/users")
+      .then(result => result.json())
+      .then(result => result.users)
+
+    console.log("fetchData result :", data)
     return data.json()
 
   }
